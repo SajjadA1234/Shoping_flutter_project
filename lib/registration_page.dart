ee4bb5daeb1fa91779633226914c2d150723ff61
SajjadA1234 <sharifsajjad43@gmail.com>
2024-12-09 19:57:46 +0330

    Regester page and etc

---
+++ lib/registration_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class RegistrationPage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registration Page'),
+      ),
+ 
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: surnameController,
+              decoration: InputDecoration(labelText: 'Surname'),
+            ),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (nameController.text.isEmpty ||
+                    surnameController.text.isEmpty ||
+                    usernameController.text.isEmpty ||
+                    passwordController.text.isEmpty ||
+                    emailController.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Please fill all fields')),
+                  );
+                  return;
+                }
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(
+                      username: usernameController.text,
+                      password: passwordController.text,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
