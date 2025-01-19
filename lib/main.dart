794135733ffa309bf41595e1f1b748e78a636b70
SajjadA1234 <sharifsajjad43@gmail.com>
2025-01-19 21:25:01 +0330

    CollaborationPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/CollaborationPage.dart';
 import 'package:flutter_application_2/Invitation%20to%20cooperate.dart';
 import 'package:flutter_application_2/categories_page.dart';
 import 'package:flutter_application_2/contact_us_page.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'My App',
       initialRoute: '/',
       routes: {
-        '/': (context) => CardGridPage(),
+        '/': (context) => HomePage(),
         '/settings': (context) =>
             SettingsPage(), // Define the SettingsPage route
         // Add other routes here if necessary
